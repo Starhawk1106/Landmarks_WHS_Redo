61f309afbfdce060efc34842fac210bcea9cf0f5
Will Stanley <whs17a@acu.edu>
2020-11-17 21:07:40 -0600

    Initial Commit

---
+++ Landmarks_WHS_Redo/Landmarks_WHS_RedoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Landmarks_WHS_RedoApp.swift
+//  Landmarks_WHS_Redo
+//
+//  Created by William Stanley on 11/17/20.
+//
+
+import SwiftUI
+
+@main
+struct Landmarks_WHS_RedoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
