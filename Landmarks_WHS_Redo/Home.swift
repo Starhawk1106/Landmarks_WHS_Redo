98d2929bef814d799bd3d6d8ff777876b7080735
Will Stanley <whs17a@acu.edu>
2020-11-19 13:22:12 -0600

    Almost Chapter 3

---
+++ Landmarks_WHS_Redo/Home.swift
@@ -0,0 +1,52 @@
+//
+//  Home.swift
+//  Landmarks_WHS_Redo
+//
+//  Created by William Stanley on 11/19/20.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarkData,
+            by: { $0.category.rawValue }
+        )
+    }
+    
+    var featured: [Landmark] {
+        landmarkData.filter { $0.isFeatured }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                FeaturedLandmarks(landmarks: featured)
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                
+                ForEach(categories.keys.sorted(), id: \.self) { key in
+                    CategoryRow(categoryName: key, items: self.categories[key]!)
+                }
+                .listRowInsets(EdgeInsets())
+            }
+            .navigationBarTitle(Text("Featured"))
+        }
+    }
+}
+
+struct FeaturedLandmarks: View {
+    var landmarks: [Landmark]
+    var body: some View {
+        landmarks[0].image.resizable()
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
